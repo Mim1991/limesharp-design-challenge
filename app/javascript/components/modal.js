59061a239eb25e425f7031f0591a675f90073368
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-03-05 17:39:54 +0000

    js clean up toggle modal

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -1,37 +1,27 @@
 const initExpansion = () => {
   const card = document.querySelectorAll(".card");
+  const cards = document.querySelector(".cards");
 
-  card.forEach((card) => {
-    card.addEventListener("click", function () {
-      const button = this.childNodes[3].lastElementChild;
-      const cards = document.querySelector(".cards");
-      const textCard = this.children[1].children[1];
-      if (
-        textCard.innerText ===
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      ) {
-        textCard.innerText =
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet consectetur adipisicing elit.";
-      } else {
-        textCard.innerText =
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
-      }
+  function toggleModal() {
+    const button = this.childNodes[3].lastElementChild;
+    const cardText = this.querySelector(".text-expand");
 
-      if (
-        button.src ===
-        "http://localhost:3000/assets/plus-6e07896c249f5d353787548226300b5868b705c2c70067c5665f888a90dc30fe.png"
-      ) {
-        console.log("plus");
-        button.src =
-          "http://localhost:3000/assets/minus-cc83297b53714b62a85a9bfdc0d09cd9452717b12914c6362a23e76fef65e78f.png";
-      } else {
-        button.src =
-          "http://localhost:3000/assets/plus-6e07896c249f5d353787548226300b5868b705c2c70067c5665f888a90dc30fe.png";
-      }
+    if (
+      button.src ===
+      "http://localhost:3000/assets/plus-6e07896c249f5d353787548226300b5868b705c2c70067c5665f888a90dc30fe.png"
+    ) {
+      button.src =
+        "http://localhost:3000/assets/minus-cc83297b53714b62a85a9bfdc0d09cd9452717b12914c6362a23e76fef65e78f.png";
+    } else {
+      button.src =
+        "http://localhost:3000/assets/plus-6e07896c249f5d353787548226300b5868b705c2c70067c5665f888a90dc30fe.png";
+    }
 
-      cards.classList.toggle("card-close");
-    });
-  });
+    cards.classList.toggle("card-close");
+    cardText.classList.toggle("text-hidden");
+  }
+
+  card.forEach((card) => card.addEventListener("click", toggleModal));
 };
 
 export { initExpansion };
